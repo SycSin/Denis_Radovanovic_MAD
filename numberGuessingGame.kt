af85fd2c13ac8b34d66b65402a5082dac1517cc9
Denis Radovanovic <radovanovic.denis@web.de>
2023-02-21 17:37:57 +0100

    Add numberGuessingGame.kt

---
+++ numberGuessingGame.kt
@@ -0,0 +1,88 @@
+package com.example.numberguessinggame
+
+fun main() {
+    val number = generateNumber()
+    print("Enter a 4-digit number: ")
+    var input = readLine().toString()
+    var inputCorrect = validateInput(input)
+    var numberFound = false
+
+    if(inputCorrect) {
+        numberFound = checkResult(number, input)
+        displayOutput(number, input)
+    }
+
+    while (!numberFound) {
+        print("Enter a 4-digit number: ")
+        input = readLine().toString()
+        inputCorrect = validateInput(input)
+        if(inputCorrect){
+            numberFound = checkResult(number, input)
+            displayOutput(number, input)
+        }
+    }
+    println("Congratulations! You have found the number!")
+}
+
+fun generateNumber(): String {
+    val digits = mutableListOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+    var number = ""
+    digits.shuffle()
+    for (i in 0 until 4) {
+        number += digits[i]
+    }
+    return number
+}
+
+fun getNrCorrectDigits(number: String,  input: String): Int {
+    var counter = 0
+
+    for(i in 0..3) {
+        for(j in 0..3){
+            if(input[i] == number[j]) {
+                counter++
+            }
+        }
+    }
+    return counter
+}
+
+fun getNrCorrectDigitsAtPosition(number: String,  input: String): Int {
+    var counter = 0
+
+    for(i in 0..3) {
+        if(input[i] == number[i]) {
+            counter++
+        }
+    }
+    return counter
+}
+
+fun validateInput(input: String): Boolean {
+    try{
+        if(Integer.parseInt(input) in 1000 .. 9999) {
+
+            return true
+        }
+        throw StringIndexOutOfBoundsException("Not a 4 digit number!")
+    }
+    catch(e: NumberFormatException) {
+        println("Invalid input! Not a number!")
+        return false
+    }
+    catch(e: StringIndexOutOfBoundsException){
+        println("Invalid input! Please enter a 4 digit number.")
+        return false
+    }
+}
+
+fun displayOutput(number: String,  input: String) {
+    println("${getNrCorrectDigits(number, input)}:${getNrCorrectDigitsAtPosition(number, input)}")
+}
+
+fun checkResult(number: String, input: String): Boolean {
+    if (Integer.parseInt(input) == Integer.parseInt(number)) {
+        return true
+    }
+    return false
+}
